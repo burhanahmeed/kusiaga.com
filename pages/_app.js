41c9f467c78680d366ecd721e214fe63208d75be
burhan ahmed <burhanahmeed@gmail.com>
2020-10-23 07:25:58 +0700

    init next website

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/personalCss.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -5,59 +5,39 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Berhan | Kusiaga</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <header>
+          <ul style={{'padding': '0', 'listStyle': 'none'}}>
+            <li className={styles['nav-menu']}>Home</li>
+            <li className={styles['nav-menu']}>Creations</li>
+            <li className={styles['nav-menu']}>About</li>
+            <li className={styles['nav-menu']}>Blogs</li>
+          </ul>
+        </header>
+        <div className="container">
+          <div className="py3 dflex mx2">
+            <div className="image-rounded image-xl p3">
+              <img src="./img/real.jpg" loading="lazy"/>
+            </div>
+            <div className="mx2">
+              <p>Burhanuddin Ahmad</p>
+              <p>Software engineer, product creator, entrepreneur</p>
+            </div>
+          </div>
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://twitter.com/burhannahm"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          A personal site by @burhannahm
         </a>
       </footer>
     </div>
