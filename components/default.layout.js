dce01ef948da24172cc829a55e7e64bb73b90a28
burhan ahmed <burhanahmeed@gmail.com>
2020-10-24 18:27:35 +0700

    add SEO meta

--- components/default.layout.js
+++ components/default.layout.js
@@ -3,10 +3,29 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 export default function Layout({ children }) {
+  let desc = "A personal site by @burhannahm. Built with Next.js."
+  let title = "@burhannahm persoanl site"
+  let previewImage = "/img/og.png"
   return (
     <div>
       <Head>
         <link rel="icon" href="/img/avatar.png" />
+        <meta charSet="utf-8" />
+ 
+        <meta name="description" content={desc}></meta>
+ 
+
+        {/* Open Graph */}
+        <meta property="og:title" content={title} key="ogtitle" />
+        <meta property="og:description" content={desc} key="ogdescription" />
+        <meta property="og:url" content="https://kusiaga.com" key="ogurl" />
+        <meta property="og:image" content={previewImage} key="ogimage" />
+        <meta property="og:site_name" content="kusiaga" key="ogsitename" />
+        
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" key="twcard" />
+        <meta name="twitter:creator" content="@burhannahm" key="twhandle" />
+
       </Head>
       <header className={styles.header}>
         <ul style={{'padding': '0', 'margin': '0', 'listStyle': 'none'}}>
