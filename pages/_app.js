0740dfedd61ec93f73b5d6b7aa891ca51f6d7efd
burhan ahmed <burhanahmeed@gmail.com>
2020-10-26 20:06:27 +0700

    add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import '../styles/personalCss.css'
 import '../styles/markdown-github.css'
+import '../styles/nprogress.css'
 import "prismjs/themes/prism-tomorrow.css";
 import {MDXProvider} from '@mdx-js/react'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const mdComponents = {
   h1: props => <h1 style={{color: '#52089d'}} {...props} />,
