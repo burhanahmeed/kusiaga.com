654c1a90fa88131d3fcf76f1c430de2aa82e4c87
Burhan Ahmed <burhanahmeed@gmail.com>
2023-11-26 21:17:45 +0700

    feat: add my work

--- pages/consultants/index.jsx
+++ pages/consultants/index.jsx
@@ -1,5 +1,23 @@
 import Head from "next/head";
 
+const myWorks = [
+  {
+    title: 'TabSquare',
+    description: 'This project is undisclosed because under Non-disclosure agreement.',
+    image: 'https://media.licdn.com/dms/image/C4E33AQEdbgnWKwqRqA/productpage-image_1128_635/0/1622827091606?e=2147483647&v=beta&t=xpbKnRz9J456-PO0YB_w_WX3maR-8nZ-aeHhW3MJVhs'
+  },
+  {
+    title: 'Rakuten RTX Property/Hotel App',
+    description: 'This project is undisclosed because under Non-disclosure agreement.',
+    image: 'https://ttgasia.2017.ttgasia.com/wp-content/uploads/sites/2/2021/08/Rakuten-Travel-Xchange-SS.jpg'
+  },
+  {
+    title: 'Rakuten Travel Experiences',
+    description: 'This project is undisclosed because under Non-disclosure agreement.',
+    image: 'https://tourteller.com/blog/wp-content/uploads/2021/08/Rakuten-Travel-Experiences-Home.jpg'
+  },
+];
+
 export default function IndexConsultant() {
   return (
     <>
@@ -56,7 +74,7 @@ export default function IndexConsultant() {
 
             <div className="py-2">
               <p>
-                Delivering a high-quality product isn't just a goal—it's a non-negotiable commitment. Your satisfaction and success are at the forefront of everything I do with very realiable and reasonable price.
+                Delivering a high-quality product isn't just a goal—it's a non-negotiable commitment. Your satisfaction and success are at the forefront of everything I do with a realiable and reasonable price.
               </p>
               <p>
                 Ready to elevate your IT game? Let's chat and explore how we can achieve remarkable results together.
@@ -68,26 +86,32 @@ export default function IndexConsultant() {
           <div className="py-8">
             <h2 className="text-center text-4xl font-bold">⚓️ Skillset</h2>
             <div class="pt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-4">
-                <div class="bg-white p-4 shadow-md">
-                    <div>
-
+                <div class="bg-white p-4 shadow-md rounded-xl text-center">
+                  <div className="m-auto p-8">
+                      <img src="./img/consultant/automated-engineering.png" alt="" />
                     </div>
-                    <p>This is content for column 1.</p>
+                    <p>Software System Design</p>
                 </div>
 
-                <div class="bg-white p-4 shadow-md">
-                    <h2 class="text-lg font-semibold mb-2">Column 2</h2>
-                    <p>This is content for column 2.</p>
+                <div class="bg-white p-4 shadow-md rounded-xl text-center">
+                  <div className="m-auto p-8">
+                      <img src="./img/consultant/computer.png" alt="" />
+                    </div>
+                    <p>Software Development</p>
                 </div>
 
-                <div class="bg-white p-4 shadow-md">
-                    <h2 class="text-lg font-semibold mb-2">Column 2</h2>
-                    <p>This is content for column 2.</p>
+                <div class="bg-white p-4 shadow-md rounded-xl text-center">
+                  <div className="m-auto p-8">
+                      <img src="./img/consultant/customer-journey-map.png" alt="" />
+                    </div>
+                    <p>Performance Optimization</p>
                 </div>
 
-                <div class="bg-white p-4 shadow-md">
-                    <h2 class="text-lg font-semibold mb-2">Column 2</h2>
-                    <p>This is content for column 2.</p>
+                <div class="bg-white p-4 shadow-md rounded-xl text-center">
+                    <div className="m-auto p-8">
+                      <img src="./img/consultant/consultant.png" alt="" />
+                    </div>
+                    <p>Software Consultancy</p>
                 </div>
             </div>
           </div>
@@ -95,35 +119,23 @@ export default function IndexConsultant() {
           <div className="py-8">
             <h2 className="text-center text-4xl font-bold">🖼 My Works</h2>
             <div className="pt-4 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
-              <div className="max-w-sm mx-auto overflow-hidden bg-white rounded-xl shadow-md">
-                  <img className="w-full h-48 object-cover" src="https://placekitten.com/401/300" alt="Card Image" />
-                  <div className="p-4">
-                      <h2 className="font-semibold text-yellow-600 mb-2">Card Title 2</h2>
-                      <p className="text-gray-600 text-xs">Description for card 2.</p>
-                  </div>
-              </div>
 
-                <div className="bg-white p-4 shadow-md">
-                    <h2 className="text-lg font-semibold mb-2">Column 2</h2>
-                    <p>This is content for column 2.</p>
-                </div>
+              {myWorks.map((val, i) => {
+                return (
+                  <MyWorkCard key={i} image={val.image} title={val.title} description={val.description} />
+                )
+              })}
 
-                <div class="bg-white p-4 shadow-md">
-                    <h2 class="text-lg font-semibold mb-2">Column 2</h2>
-                    <p>This is content for column 2.</p>
-                </div>
-
-                <div class="bg-white p-4 shadow-md">
-                    <h2 class="text-lg font-semibold mb-2">Column 2</h2>
-                    <p>This is content for column 2.</p>
-                </div>
             </div>
           </div>
 
           <div className="py-8 pb-16">
             <h2 className="text-center text-4xl font-bold">💳 Get Quotation</h2>
+            <div className="text-center p-4">
+              <p>Contact me at <a className="text-yellow-600" href="mailto:burhanahmeed@icloud.com">burhanahmeed@icloud.com</a> or You can schedule a meeting with me <a className="text-yellow-600" href="https://calendly.com/burhannahm/30min">here</a>.</p>
+            </div>
 
-              <div class="mb-4">
+              {/* <div class="mb-4">
                   <label for="name" class="block text-gray-600">Name</label>
                   <input type="text" id="name" name="name" class="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500" />
               </div>
@@ -136,9 +148,9 @@ export default function IndexConsultant() {
               <div class="mb-4">
                   <label for="hired-as" class="block text-gray-600">Hired Me As</label>
                   <select id="hired-as" name="hired-as" class="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500">
-                      <option value="CTO">CTO</option>
+                      <option value="CTO">Fractional CTO</option>
                       <option value="engineer">Engineer</option>
-                      <option value="lead">Lead</option>
+                      <option value="lead">Team/Tech Lead</option>
                       <option value="other">Other IT Development</option>
                   </select>
               </div>
@@ -148,7 +160,7 @@ export default function IndexConsultant() {
                   <textarea id="message" name="message" rows="4" class="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"></textarea>
               </div>
 
-              <button class="w-full px-4 py-2 bg-yellow-600 text-white rounded hover:bg-yellow-700 focus:outline-none">Get Quotation</button>
+              <button class="w-full px-4 py-2 bg-yellow-600 text-white rounded hover:bg-yellow-700 focus:outline-none">Get Quotation</button> */}
           </div>
 
           {/* Footer  */}
@@ -158,5 +170,19 @@ export default function IndexConsultant() {
         </div>
       </div>
     </>
+  )
+}
+
+const MyWorkCard = ({
+  image, title, description
+}) => {
+  return (
+    <div className="max-w-sm mx-auto overflow-hidden bg-white rounded-xl shadow-md">
+      <img className="w-full h-48 object-cover" src={image} alt="Card Image" />
+      <div className="p-4">
+        <h2 className="font-semibold text-yellow-600 mb-2">{title}</h2>
+        <p className="text-gray-600 text-xs">{description}</p>
+      </div>
+    </div>
   )
 }
